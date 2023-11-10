1db5b3917b9503a8f9853b8389ed093a5b624c6a
niosni <niklas.niemela@hotmail.com>
2023-11-10 21:00:06 +0200

    Add filter/search input field for stations and style it

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,16 @@ interface Station {
 
 function App() {
   const [stations, setStations] = useState<Station[]>([])
+  const [filterValue, setFilterValue] = useState('')
+  const [stationsToShow, setStationsToShow] = useState<Station[]>([])
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await stationService.getAll()
-        setStations(response.sort((a: Station, b: Station) => a.station_name.localeCompare(b.station_name)))
-
+        const sortedStations = response.sort((a: Station, b: Station) => a.station_name.localeCompare(b.station_name))
+        setStations(sortedStations)
+        setStationsToShow(sortedStations)
       } catch (error) {
         console.error(error)
       }
@@ -30,22 +33,45 @@ function App() {
 
   const StationsList = () => {
     return (
-      <div>
-        <h2>Citybike stations</h2>
-        {stations.length > 0 ? '' : 'Loading '}
-        {stations.map(station =>
+      <>
+        {stationsToShow.length > 0 ? '' : 'None match '}
+        {stationsToShow.map(station =>
           <StationCard
             key={station.id}
             station={station}
           />
         )}
+      </>
+    )
+  }
+  const handleFiltering = (event: React.ChangeEvent<HTMLElement>) => {
+    const target = event.target as HTMLButtonElement
+    const newFilterValue = String(target.value)
+    setFilterValue(newFilterValue)
+    const filteredStations = stations.filter(station =>
+      station.station_name.toLowerCase().includes(newFilterValue.toLowerCase())
+    )
+    setStationsToShow(filteredStations)
+  }
+
+  const StationsFilter = () => {
+    return (
+      <div className='filter-station'>
+      Find stations:
+        <input
+          type='text'
+          onChange={handleFiltering}
+          value={filterValue}
+        />
       </div>
     )
   }
 
   return (
     <>
-      <div>
+      <h2>Citybike stations</h2>
+      {StationsFilter()}
+      <div className="stations-list">
         {StationsList()}
       </div>
     </>
