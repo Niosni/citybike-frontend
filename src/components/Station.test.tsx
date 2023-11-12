13077358b4fbd8c7c8aa8a00f06535a9052b3322
niosni <niklas.niemela@hotmail.com>
2023-11-12 17:36:17 +0200

    Add user-event to test user actions

--- src/components/Station.test.tsx
+++ src/components/Station.test.tsx
@@ -1,19 +1,32 @@
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Station from './Station'
 import { expect, jest, test } from '@jest/globals'
 
+const mockHandler = jest.fn()
+
+const station = {
+  id: 283,
+  station_name: 'Alakiventie',
+  station_address: 'Alakiventie 4',
+  coordinate_x: '25.0772822325582',
+  coordinate_y: '60.2198397844715'
+}
+
 test('renders content', () => {
-  const station = {
-    id: 283,
-    station_name: 'Alakiventie',
-    station_address: 'Alakiventie 4',
-    coordinate_x: '25.0772822325582',
-    coordinate_y: '60.2198397844715'
-  }
-
-  render(<Station station={station} showButton={jest.fn()} />)
-
+  render(<Station station={station} showButton={mockHandler} />)
   const element = screen.getByText('Alakiventie')
+
   expect(element).toBeDefined()
+})
+
+test('Clicking the "show details" -button calls event handler once', async () => {
+  render(<Station station={station} showButton={mockHandler} />)
+  const user = userEvent.setup()
+
+  const button = screen.getByText('Show details')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
